6ead7362274715d6cb428f23b5e8772fdccbf001
mikasa <mikasanow@gmail.com>
2016-02-10 00:33:31 +0900

    [add] 新規登録
    新規登録

---
+++ ArchiveFiles.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# リストファイルに従いファイルを圧縮する
+
+TARGET_DIR=/home/pi/work/log
+OUTPUT_DIR=/home/pi/work
+OUTPUT=${OUTPUT_DIR}/`date +%Y%m%d-%H%M`_.tar.gz
+
+# 圧縮するファイル(.log)のリストを作成
+find ${TARGET_DIR} -iregex ".*\.log$" > ${OUTPUT_DIR}/file.list
+
+# リストに従って圧縮(tar.gz)
+tar -zcf ${OUTPUT} -T ${TARGET_DIR}/file.list
+
+# リストファイルを削除
+# rm -rf ${OUTPUT_DIR}/file.list
+
+exit 0
